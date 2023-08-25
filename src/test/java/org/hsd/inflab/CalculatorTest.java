cb7b8de12186fab6a4b26c68dadeb383e9d22ec8
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2023-08-25 13:02:00 +0200

    format test (#1)

--- src/test/java/org/hsd/inflab/CalculatorTest.java
+++ src/test/java/org/hsd/inflab/CalculatorTest.java
@@ -11,19 +11,19 @@ class CalculatorTest {
 
     Calculator calculator;
 
-    @BeforeEach                                         
+    @BeforeEach
     void setUp() {
         calculator = new Calculator();
     }
 
-    @Test                                               
-    @DisplayName("Simple multiplication should work")   
+    @Test
+    @DisplayName("Simple multiplication should work")
     void testMultiply() {
-        assertEquals(20, calculator.multiply(4, 5),     
-                "Regular multiplication should work");  
+        assertEquals(20, calculator.multiply(4, 5),
+                "Regular multiplication should work");
     }
 
-    @RepeatedTest(5)                                    
+    @RepeatedTest(5)
     @DisplayName("Ensure correct handling of zero")
     void testMultiplyWithZero() {
         assertEquals(0, calculator.multiply(0, 5), "Multiple with zero should be zero");
